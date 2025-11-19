8b271306e22dbb3fd67cb3fde8a87d8890e5af03
Nischay <nischaydilhor@gmail.com>
2025-11-19 16:03:59 +0530

    Add subscription controller with functionality for toggling subscriptions and retrieving subscriber lists

---
+++ src/controllers/subscription.controller.js
@@ -0,0 +1,84 @@
+import mongoose, { isValidObjectId } from "mongoose"
+import { User } from "../models/user.model.js"
+import { Subscription } from "../models/subscription.model.js"
+import { ApiError } from "../utils/apiError.js"
+import { ApiResponse } from "../utils/apiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+
+const toggleSubscription = asyncHandler(async (req, res) => {
+    const { channelId } = req.params
+    const userId = req.user?._id
+
+    if (!channelId) {
+    throw new ApiError(400, "Channel Id is required");
+    }
+
+
+    const alreadySubscribed = await Subscription.findOne({ subscriber: userId, channel: channelId })
+
+    if (alreadySubscribed) {
+        await alreadySubscribed.deleteOne()
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(200, {}, "Channel unsubscribed")
+            )
+    }
+
+    await Subscription.create({
+        subscriber: userId,
+        channel: channelId
+    })
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, {}, "Channel subscribed")
+        )
+
+})
+
+// controller to reutnr subscriber list of a channel
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+    const {channelId} = req.params
+
+    if (!channelId) {
+        throw new ApiError(400, "Channel Id is required")
+    }
+
+    const subscribers = await Subscription.find({
+        channel: channelId
+    }).populate({ path: "subscriber" })
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, { count: subscribers.length }, "subscribers")
+        )
+
+})
+
+// controller to return channel list to which user has subscribed
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+    const {subscriberId} = req.params
+
+    if (!subscriberId) {
+        throw new ApiError(400, "Channel Id is required")
+    }
+
+    const subscribedChannel = await Subscription.find({
+        subscriber: subscriberId
+    }).populate({ path: "channel" })
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, { count: subscribedChannel.length }, "subscribed channel")
+        )
+})
+
+export {
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels
+}
