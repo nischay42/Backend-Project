76d7f4c10dda9d91be2f579e407905f402bbcdb3
Nischay <nischaydilhor@gmail.com>
2025-11-25 18:34:34 +0530

    Initialize frontend with React

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+    <div className="h-screen">
+    <h1 className="text-center text-2xl">Hello World</h1>
+
+    </div>
+    </>
+  )
+}
+
+export default App
