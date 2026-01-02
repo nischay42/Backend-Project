640c111dce031309ea1bfc163cc27ee7afd244c2
Nischay <nischaydilhor@gmail.com>
2026-01-02 19:56:14 +0530

    Add API functions for liking videos, comments, and tweets

---
+++ Frontend/src/api/like.api.ts
@@ -0,0 +1,47 @@
+import api from './axios'
+
+type videoLike = {
+    videoId: string,
+    type: string
+}
+const toggoleVideoLike = async (params: videoLike) => {
+    
+    const res = await api.post(`/v/:${params.videoId}/:${params.type}`)
+    return res.data
+
+}
+
+type commentLike = {
+    commentId: string,
+    type: string
+}
+
+const toggoleCommentLike = async (params: commentLike) => {
+    
+    const res = await api.post(`/c/:${params.commentId}/:${params.type}`)
+    return res.data
+}
+
+type tweeetLike = {
+    tweetId: string,
+    type: string
+}
+
+const toggoleTweetLike = async (params: tweeetLike) => {
+    
+    const res = await api.post(`/t/:${params.tweetId}/:${params.type}`)
+    return res.data
+}
+
+const getLikedVideos = async () => {
+    
+    const res = await api.post("/videos")
+    return res.data
+}
+
+export {
+    toggoleVideoLike,
+    toggoleCommentLike,
+    toggoleTweetLike,
+    getLikedVideos
+}
