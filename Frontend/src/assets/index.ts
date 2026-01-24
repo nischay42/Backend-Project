943cc646d858e2be4e84073029213298d1c05549
Nischay <nischaydilhor@gmail.com>
2026-01-24 18:02:17 +0530

    Add asset imports for SVG icons

---
+++ Frontend/src/assets/index.ts
@@ -0,0 +1,19 @@
+import collection from './collection.svg'
+import history from './history.svg'
+import home from './home-line.svg'
+import like from './like.svg'
+import content from './my content.svg'
+import setting from './setting.svg'
+import subscriber from './subscriber.svg'
+import support from './support.svg'
+
+export {
+    collection,
+    history,
+    home,
+    like,
+    content,
+    setting,
+    subscriber,
+    support
+}
