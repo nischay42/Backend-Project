e4b4d3e640de9d8b38490a5c21bbd25aab6d5dd2
Nischay <nischaydilhor@gmail.com>
2026-01-24 18:05:23 +0530

    Refactor main.tsx to integrate Redux and PersistGate for state management

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,22 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store } from './app/store.ts'
+import { persistStore } from 'redux-persist'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const persistor = persistStore(store)
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 )
