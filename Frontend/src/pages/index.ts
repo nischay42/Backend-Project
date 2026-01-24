09cae387e07ce6445f0796901ffa3c0ede3b7564
Nischay <nischaydilhor@gmail.com>
2026-01-24 17:57:42 +0530

    Create index file to export main components for the application

---
+++ Frontend/src/pages/index.ts
@@ -0,0 +1,23 @@
+import History from "./History/History"
+import Collection from "./Collection/Collection"
+import Home from "./Home"
+import LikeVideo from "./Like Video/LikeVideo"
+import MyContent from "./My Content/MyContent"
+import Setting from "./Setting/Setting"
+import Support from "./Support/Support"
+import Subscribers from "./Subscribers/Subscribers"
+import Login from "./Login"
+import Signup from "./Signup"
+
+export {
+    History,
+    Collection,
+    Home,
+    LikeVideo,
+    MyContent,
+    Setting,
+    Support,
+    Subscribers,
+    Login,
+    Signup
+}
