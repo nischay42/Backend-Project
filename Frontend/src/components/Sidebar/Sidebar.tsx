097f41b5630425456ef6ddf46d25c3fe15b0b258
Nischay <nischaydilhor@gmail.com>
2026-01-26 17:24:49 +0530

    add link to navigate to the page

--- Frontend/src/components/Sidebar/Sidebar.tsx
+++ Frontend/src/components/Sidebar/Sidebar.tsx
@@ -9,16 +9,19 @@ import {
     support
 } from "../../assets/index.ts"
 import Button from './Button.tsx'
+import { useAppSelector } from "../../app/hooks.ts"
+import { Link } from "react-router-dom"
 
 const Sidebar = () => {
 
+const { isAuthenticated } = useAppSelector((state) => state.auth)
 const sidebarTabs = [
-  { label: "Home", icon: home, path: "/", group: "top" },
-  { label: "Like Video", icon: like, path: "/liked", group: "top" },
-  { label: "History", icon: history, path: "/history", group: "top" },
-  { label: "My Content", icon: content, path: "/content", group: "top" },
-  { label: "Collection", icon: collection, path: "/collection", group: "top" },
-  { label: "Subscribers", icon: subscriber, path: "/subscribers", group: "top" },
+  { label: "Home", icon: home, path: "/", group: "top", isVisible: true },
+  { label: "Liked Video", icon: like, path: "/liked-videos", group: "top", isVisible: isAuthenticated },
+  { label: "History", icon: history, path: "/history", group: "top", isVisible: isAuthenticated },
+  { label: "My Channel", icon: content, path: "/channel", group: "top", isVisible: isAuthenticated },
+  { label: "Collection", icon: collection, path: "/collection", group: "top", isVisible: isAuthenticated },
+  { label: "Subscribers", icon: subscriber, path: "/subscribers", group: "top", isVisible: isAuthenticated },
 
   { label: "Support", icon: support, path: "/support", group: "bottom" },
   { label: "Setting", icon: setting, path: "/setting", group: "bottom" },
@@ -32,24 +35,29 @@ const sidebarTabs = [
   return (
     <div className='flex flex-col justify-between h-full w-1/6 border-white border-r p-2'>
       <ul>
-        {topTabs.map((item) => (
-          <li
-            key={item.label}
-            // path={item.path}
-          >
-            <Button icon={item.icon} label={item.label} />
-          </li>
-        ))}
+        {topTabs.map((item) => 
+          (item.isVisible ? 
+              <li
+              key={item.label}
+              >
+                <Link to={item.path}>
+                    <Button icon={item.icon} label={item.label} />
+                </Link>
+              </li>
+           : 
+           null)
+        )}
       </ul>
       <ul>
         
         {bottomTabs.map((item) => (
           <li
-            key={item.label}
-            // path={item.path}
-          >
-            <Button icon={item.icon} label={item.label} />
-          </li>
+              key={item.label}
+            >
+              <Link to={item.path}>
+                  <Button icon={item.icon} label={item.label} />
+              </Link>
+            </li>
         ))}
       </ul>
     </div>
