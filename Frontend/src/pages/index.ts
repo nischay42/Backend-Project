43f594f4dc8ceac3d30779168b76a69ecfeefbcc
Nischay <nischaydilhor@gmail.com>
2026-02-10 02:41:08 +0530

    feat: import Following and VideoPlayer components in index.ts

--- Frontend/src/pages/index.ts
+++ Frontend/src/pages/index.ts
@@ -2,22 +2,24 @@ import History from "./History"
 import Collection from "./Collection"
 import Home from "./Home"
 import LikedVideo from "./LikedVideo"
-import MyChannel from "./MyChannel"
 import Setting from "./Setting"
 import Support from "./Support"
 import Subscribers from "./Subscribers"
 import Login from "./Login"
 import Signup from "./Signup"
+import Following from './channel/Following'
+import VideoPlayer from "./VideoPlayer"
 
 export {
     History,
     Collection,
     Home,
     LikedVideo,
-    MyChannel,
     Setting,
     Support,
     Subscribers,
     Login,
-    Signup
+    Signup,
+    Following,
+    VideoPlayer
 }
