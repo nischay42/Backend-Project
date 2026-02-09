ad78f5e51aed7c9a564f9896874038ed3c653823
Nischay <nischaydilhor@gmail.com>
2026-02-10 02:28:55 +0530

    refactor: streamline imports and clean up unused code in App component

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,19 +1,18 @@
 import { getHealthStatus } from "./api/healthCheck.api"
-import { userLogin, userLogout } from "./api/user.api";
-import Sidebar from "./components/Sidebar/Sidebar";
-import NavBar from "./components/navigation/Navbar";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect } from 'react'
 import { useAppDispatch } from "./app/hooks";
 import { fetchCurrentUser } from "./features/auth/authSlice";
-import ProtectedRoute from "./components/layout/ProtectedRoute";
-import MainLayout from "./components/layout/MainLayout";
-import AuthLayout from "./components/layout/AuthLayout";
-import { Home, Login, Signup, MyChannel, LikedVideo, Setting, Support, History, Subscribers } from "./pages/index";
+import { ProtectedRoute, MainLayout, AuthLayout, ChannelLayout} from "./layout/index";
+import { Home, Login, Signup, LikedVideo, Setting, Support, History, Subscribers, Following, VideoPlayer } from "./pages/index";
 import { ToastProvider } from "./context/ToastContext";
+import Videos from "./pages/channel/Videos";
+import Playlist from "./pages/channel/Playlist";
+import Tweets from "./pages/channel/Tweets";
+import UploadVideo from "./pages/channel/UploadVideo";
 
 function App() {
+  const dispatch = useAppDispatch()
 
   useEffect(() => {
     getHealthStatus()
@@ -21,22 +20,6 @@ function App() {
       .catch((err) => console.log("API error", err))
   }, [])
   
-
-  // function login() {
-  //   const user = {email: "one@gmai.com", password: "123456789"}
-  //   userLogin(user)
-  //     .then((data) => console.log("Login successfully", data))
-  //     .catch((err) => console.log("Login Error", err))
-  // }
-
-  // function logout() {
-  //   userLogout()
-  //     .then((data) => console.log("logout successfully",data))
-  //     .catch((err) => console.log("logout err", err))
-  // }
-
-  const dispatch = useAppDispatch()
-
   useEffect(() => {
     dispatch(fetchCurrentUser())
     
@@ -56,7 +39,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/support" element={<Support />} />
             <Route path="/setting" element={<Setting />} />
-            {/* <Route /> */}
+            <Route path="/watch" element={<VideoPlayer />} />
           </Route>
           
           <Route element={
@@ -65,8 +48,18 @@ function App() {
             </ProtectedRoute>
           }>
             <Route path="/liked-videos" element={<LikedVideo />}></Route>
+            <Route path="/:username" element={<ChannelLayout />}>
+              <Route index element={<Videos />} />
+              <Route path="playlist" element={<Playlist />} />
+              <Route path="tweets" element={<Tweets />} />
+              <Route path="subscribers" element={<Following />} />
+              <Route path="upload" element={
+                <ProtectedRoute>
+                  <UploadVideo />
+                </ProtectedRoute>} 
+              />
+            </Route>
             <Route path="/history" element={<History />}></Route>
-            <Route path="/channel" element={<MyChannel />}></Route>
             <Route path="/subscribers" element={<Subscribers />}></Route>
           </Route>
 
@@ -74,20 +67,6 @@ function App() {
         </Routes>
       </BrowserRouter>
     </ToastProvider>
-    // <>
-    // {/* <div className="h-screen">
-    // <h1 className="text-center text-2xl">Hello World</h1>
-    // <button onClick={()=> login()}>Login</button>
-    // <button onClick={()=> logout()}>Logout</button>
-
-    // </div> */}
-    // <main className="h-screen bg-black text-white">
-    //   {/* <NavBar />
-    //   <Sidebar /> */}
-    //   <MainLayout />
-    // </main>
-
-    // </>
   )
 }
 
