79a2fdfd602e20eb85859e05bab404baa7d9c8f8
Nischay <nischaydilhor@gmail.com>
2026-02-10 03:03:58 +0530

    feat: add index file to export layout components for better structure

---
+++ Frontend/src/layout/index.ts
@@ -0,0 +1,11 @@
+import ProtectedRoute from "./ProtectedRoute";
+import MainLayout from "./MainLayout";
+import AuthLayout from "./AuthLayout";
+import ChannelLayout from "./ChannelLayout";
+
+export {
+    ProtectedRoute,
+    MainLayout,
+    AuthLayout,
+    ChannelLayout
+}
