a49e1d52642d2ab22876c1adc689a6ff7844d90c
Nischay <nischaydilhor@gmail.com>
2026-03-26 03:23:42 +0530

    feat(pages): update LikedVideo, index
    - add LikedVideo: fetchLikedVideos
    - add index: import History from "./History"

--- Frontend/src/pages/LikedVideo.tsx
+++ Frontend/src/pages/LikedVideo.tsx
@@ -1,8 +1,70 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { getLikedVideos } from '../api/like.api'
+import { useToastContext } from '../context/ToastContext'
+import WatchVideoCard from '../components/video/WatchVideoCard'
+import { Search } from 'lucide-react'
+import WatchVideoCardSkeleton from '../components/video/WatchVideoCardSkeleton'
 
 const LikedVideo = () => {
+  const [likedVideos, setLikedVideos] = useState<any[]>([])
+  const [likedVideosCount, setLikedVideosCount] = useState(0)
+  const [dislikedVideosCount, setDislikedVideosCount] = useState(0)
+  const [isLoading, setIsLoading] = useState(true)
+  const toast = useToastContext()
+
+  useEffect(() => {
+    const fetchLikedVideos = async () => {
+      try {
+        setIsLoading(true)
+        const res = await getLikedVideos()
+        setLikedVideos(res.data.likedVideos)
+        setLikedVideosCount(res.data?.LikedCount)
+        setDislikedVideosCount(res.data?.DisliedCount)
+      } catch (error) {
+        console.log('Failed to fetch liked videos', error);
+        toast.error('Failed to fetch liked videos')
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchLikedVideos()
+  }, [])
+
   return (
-    <div>LikedVideo</div>
+     <div className="flex w-full justify-center lg:justify-end lg:p-7">  
+      <div className="flex flex-col-reverse lg:flex-row lg:w-[85vw]">
+        <div className="lg:w-[45vw] w-full flex flex-col p-2 lg:p-0 gap-2">
+          {isLoading
+            ? (
+                Array.from({ length: 7 }).map((_, i) => (
+                <WatchVideoCardSkeleton key={i} videoWidth="lg:w-2/5 w-1/2" />
+            ))
+            ) : (<>{likedVideos && likedVideos.map((video) => (
+            <WatchVideoCard
+              key={video?._id}
+              fullname={video.owner.fullname}
+              username={video.owner.username}
+              thumbnail={video.thumbnail}
+              videoFile={video.videoFile}
+              avatar={video.owner.avatar}
+              duration={video.duration}
+              title={video.title}
+              views={video.views}
+              createdAt={video.createdAt}
+              videoId={video._id}
+            />
+          ))}</>)}
+        </div>
+        <div className="flex flex-col lg:pt-35 lg:pl-30 px-3 lg:px-0">
+          <div className="flex items-center gap-2 border-b py-2 my-5">
+            <Search className='text-lg cursor-pointer' />
+            <input placeholder='Search liked video' className='border-none focus:outline-none' />
+          </div>
+          <p>Liked videos: {likedVideosCount}</p>
+          <p>Disliked videos: {dislikedVideosCount}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- Frontend/src/pages/index.ts
+++ Frontend/src/pages/index.ts
@@ -2,24 +2,28 @@ import History from "./History"
 import Collection from "./Collection"
 import Home from "./Home"
 import LikedVideo from "./LikedVideo"
-import Setting from "./Setting"
 import Support from "./Support"
-import Subscribers from "./Subscribers"
 import Login from "./Login"
 import Signup from "./Signup"
 import Following from './channel/Following'
 import VideoPlayer from "./VideoPlayer"
+import Dashboard from "./Dashboard"
+import WatchLater from "./WatchLater"
+import PlaylistDetail from "./PlaylistDetail"
+import SearchResult from "./SearchResult"
 
 export {
     History,
     Collection,
     Home,
     LikedVideo,
-    Setting,
     Support,
-    Subscribers,
     Login,
     Signup,
     Following,
-    VideoPlayer
+    VideoPlayer,
+    Dashboard,
+    WatchLater,
+    PlaylistDetail,
+    SearchResult
 }
