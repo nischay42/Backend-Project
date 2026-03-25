cf3a6e71c75958b607071cd41f6a8c85f361393a
Nischay <nischaydilhor@gmail.com>
2026-03-25 14:24:27 +0530

    add  getVideoLikes,  getCommentsLikes,  getTweetLikes methods

--- Backend/src/controllers/like.controller.js
+++ Backend/src/controllers/like.controller.js
@@ -8,12 +8,12 @@ import { asyncHandler } from '../utils/asyncHandler.js'
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const { videoId, type } = req.params
     const userId = req.user?._id
-
+    
     if (!isValidObjectId(videoId) || !type) {
         throw new ApiError(400, "video Id or type is missing")
     }
 
-    const existingReaction = await Like.findOne({ video: videoId, user: userId})
+    const existingReaction = await Like.findOne({ video: videoId, owner: userId })
 
     if (existingReaction) {
         if (existingReaction.reactionType === type) {
@@ -57,7 +57,7 @@ const toggleCommentsLike = asyncHandler(async (req, res) => {
         throw new ApiError(400, "comment Id or type is missing")
     }
 
-    const existingReaction = await Like.findOne({ comment: commentId, user: userId})
+    const existingReaction = await Like.findOne({ comment: commentId, owner: userId})
 
     if (existingReaction) {
         if (existingReaction.reactionType === type) {
@@ -98,11 +98,11 @@ const toggleTweenLike = asyncHandler(async (req, res) => {
     const { tweetId, type } = req.params
     const userId = req.user?._id
 
-    if (!isValidObjectId(userId)) {
+    if (!isValidObjectId(userId) || !type) {
         throw new ApiError(400, "User Id is missing for tweet")
     }
 
-    const existingReaction = await Like.findOne({ tweet: tweetId, user: userId})
+    const existingReaction = await Like.findOne({ tweet: tweetId, owner: userId})
 
     if (existingReaction) {
         if (existingReaction.reactionType === type) {
@@ -148,33 +148,239 @@ const getLikedVideos = asyncHandler(async (req, res) => {
     }
 
     const reactions = await Like.find({
-        user: userId,
+        owner: userId,
         reactionType: "like",
         video: { $exists: true, $ne: null}
     }).populate({
         path: "video",
-        populate: { path: "owner", select: "username fullname"}
+        populate: { path: "owner", select: "avatar username fullname"}
     })
-
+    
     const likedVideos = reactions
     .map(reac => reac.video)
     .filter(vide => vide !== null)
+    
+    const dislikedVideos = await Like.find({
+        owner: userId,
+        reactionType: "dislike",
+        video: { $exists: true, $ne: null}
+    })
 
     return res
         .status(200)
         .json(
-            new ApiResponse(200, {count: likedVideos.length, likedVideos}, "All liked videos fetched")
+            new ApiResponse(200, {LikedCount: likedVideos.length, DisliedCount: dislikedVideos.length, likedVideos}, "All liked videos fetched")
         )
+})
 
+const getVideoLikes = asyncHandler(async (req, res) => {
+    const { videoId } = req.params
+    const userId = req.user?._id
+
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid videoId")
+    }
+
+    const likes = await Like.aggregate([
+        {
+            $match: {
+                video: new mongoose.Types.ObjectId(videoId)
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                likesCount: {
+                    $sum: { $cond: [{ $eq: ['$reactionType', 'like'] }, 1, 0 ] }
+                },
+                dislikesCount: {
+                    $sum: { $cond: [{ $eq: ['$reactionType', 'dislike'] }, 1, 0] }
+                },
+                userReaction: {
+                    $max: {
+                        $cond: {
+                            if: {
+                                $and: [
+                                    { $ne: [userId ?? null, null] },
+                                    {
+                                        $eq: [
+                                            '$owner',
+                                            userId ? new mongoose.Types.ObjectId(userId) : null
+                                        ]
+                                    }
+                                ]
+                            },
+                            then: '$reactionType',
+                            else: null
+                        }
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                likesCount: 1,
+                dislikesCount: 1,
+                userReaction: 1
+            }
+        }
+    ])
+
+    const result = likes[0] || {
+        likesCount: 0,
+        dislikesCount: 0,
+        userReaction: null
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, result, "Video likes fetched")
+        )
 })
 
 
+const getCommentsLikes = asyncHandler(async (req, res) => {
+    const { commentId } = req.params
+    const userId = req.user?._id
 
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid videoId")
+    }
 
+    const likes = await Like.aggregate([
+        {
+            $match: {
+                comment: new mongoose.Types.ObjectId(commentId)
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                likesCount: {
+                    $sum: { $cond: [{ $eq: ['$reactionType', 'like'] }, 1, 0 ] }
+                },
+                dislikesCount: {
+                    $sum: { $cond: [{ $eq: ['$reactionType', 'dislike'] }, 1, 0] }
+                },
+                userReaction: {
+                    $max: {
+                        $cond: {
+                            if: {
+                                $and: [
+                                    { $ne: [userId ?? null, null] },
+                                    {
+                                        $eq: [
+                                            '$owner',
+                                            userId ? new mongoose.Types.ObjectId(userId) : null
+                                        ]
+                                    }
+                                ]
+                            },
+                            then: '$reactionType',
+                            else: null
+                        }
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                likesCount: 1,
+                dislikesCount: 1,
+                userReaction: 1
+            }
+        }
+    ])
+
+    const result = likes[0] || {
+        likesCount: 0,
+        dislikesCount: 0,
+        userReaction: null
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, result, "Video likes fetched")
+        )
+})
+
+const getTweetLikes = asyncHandler(async (req, res) => {
+    const { tweetId } = req.params
+    const userId = req.user?._id
+
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid videoId")
+    }
+
+    const likes = await Like.aggregate([
+        {
+            $match: {
+                tweet: new mongoose.Types.ObjectId(tweetId)
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                likesCount: {
+                    $sum: { $cond: [{ $eq: ['$reactionType', 'like'] }, 1, 0 ] }
+                },
+                dislikesCount: {
+                    $sum: { $cond: [{ $eq: ['$reactionType', 'dislike'] }, 1, 0] }
+                },
+                userReaction: {
+                    $max: {
+                        $cond: {
+                            if: {
+                                $and: [
+                                    { $ne: [userId ?? null, null] },
+                                    {
+                                        $eq: [
+                                            '$owner',
+                                            userId ? new mongoose.Types.ObjectId(userId) : null
+                                        ]
+                                    }
+                                ]
+                            },
+                            then: '$reactionType',
+                            else: null
+                        }
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                likesCount: 1,
+                dislikesCount: 1,
+                userReaction: 1
+            }
+        }
+    ])
+
+    const result = likes[0] || {
+        likesCount: 0,
+        dislikesCount: 0,
+        userReaction: null
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, result, "Video likes fetched")
+        )
+})
 
 export {
     toggleCommentsLike,
     toggleTweenLike,
     toggleVideoLike,
-    getLikedVideos
+    getLikedVideos,
+    getVideoLikes,
+    getCommentsLikes,
+    getTweetLikes
 }
