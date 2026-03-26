512a8d798ed82e982956ffdc7a7b23995655fed4
Nischay <nischaydilhor@gmail.com>
2026-03-26 14:14:48 +0530

    build(vite.config): update vite.config
    - update vite.config

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api/v1": "http://localhost:8000" ,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    server: {
+      proxy: {
+        "/api/v1": env.VITE_API_URL,
+      },
     },
-  },
-  plugins: [react(),
-    tailwindcss(),
-  ],
+    plugins: [react(),
+      tailwindcss(),
+    ],
+  }
 })
