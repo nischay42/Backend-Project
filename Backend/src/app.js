bab5d9e3a78da64b1c930cd4b1934d6f2c1549d1
Nischay <nischaydilhor@gmail.com>
2026-03-26 17:08:44 +0530

    refactor(app): update import cookieParser from 'cookie-parser'
    - restructure app: import cookieParser from 'cookie-parser'

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser'
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: 'https://play-01.vercel.app',
     credentials: true
 }))
 
